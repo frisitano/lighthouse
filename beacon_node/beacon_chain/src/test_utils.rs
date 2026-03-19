44c1b17cdc9909e9e1348248c9be7535cb732c58
Nova <nova.tau.assistant@gmail.com>
2026-03-19 05:19:59 +0000

    Revert "fix: replace deprecated try_next() with try_recv().ok()"
    This reverts commit d317436deec15f5adc9151ced5adaace4a965a1f.

--- beacon_node/beacon_chain/src/test_utils.rs
+++ beacon_node/beacon_chain/src/test_utils.rs
@@ -804,7 +804,11 @@ where
     pub fn shutdown_reasons(&self) -> Vec<ShutdownReason> {
         let mutex = self.shutdown_receiver.clone();
         let mut receiver = mutex.lock();
-        std::iter::from_fn(move || receiver.try_recv().ok())
+        std::iter::from_fn(move || match receiver.try_next() {
+            Ok(Some(s)) => Some(s),
+            Ok(None) => panic!("shutdown sender dropped"),
+            Err(_) => None,
+        })
         .collect()
     }
 
